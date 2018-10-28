58b4cef4d83f3792d73e02d7cfcae493c1e3b2ae
Matteo Muscella <matteo.muscella@mail.com>
2018-10-28 16:46:15 +0100

    Updates manifest to Swift 4.2

--- package.swift
+++ package.swift
@@ -1,6 +1,24 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
     name: "Mustard",
-    dependencies: []
+    products: [
+      .library(
+        name: "Mustard",
+        targets: ["Mustard"]),
+    ],
+    dependencies: [],
+    targets: [
+      .target(
+        name: "Mustard",
+        dependencies: []),
+      .testTarget(
+        name: "MustardTests",
+        dependencies: [
+          "Mustard",
+        ])
+    ],
+    swiftLanguageVersions: [.v4, .v4_2]
 )
